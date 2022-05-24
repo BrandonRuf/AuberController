a725c05b549f330edacd69a99c1835f8ef5bf035
BrandonRuf <91632371+BrandonRuf@users.noreply.github.com>
2022-05-24 10:51:16 -0400

    Additional comments

--- auber_controller.py
+++ auber_controller.py
@@ -1,3 +1,10 @@
+"""
+A Graphical interface for controlling the Auber SYL53X2P.
+ 
+Written by Brandon Ruffolo (2022).
+
+"""
+
 import spinmob.egg   as _egg
 import spinmob       as _s
 import time          as _time
@@ -11,11 +18,6 @@ from auber_controller_api    import auber_syl53x2p_api
 
 # GUI settings
 _s.settings['dark_theme_qt'] = True
-
-# Defined fonts
-style_1 = 'font-size: 14pt; font-weight: bold; color: '+('mediumspringgreen' if _s.settings['dark_theme_qt'] else 'blue')
-style_2 = 'font-size: 17pt; font-weight: bold; color: '+('white'             if _s.settings['dark_theme_qt'] else 'red')
-style_3 = 'font-size: 17pt; font-weight: bold; color: '+('cyan'              if _s.settings['dark_theme_qt'] else 'purple')
 
 # 
 PROGRAM_STEPS = 10
@@ -638,11 +640,11 @@ class auber_syl53x2p(serial_gui_base):
         self.grid_bot.disable()
         
         # Add NumberBox and label for the current measured temperature from the Auber
-        self.grid_upper_mid.add(_g.Label('Measured Temperature:'), alignment=1).set_style(style_2)
+        self.grid_upper_mid.add(_g.Label('Measured Temperature:'), alignment=1).set_style('font-size: 17pt; font-weight: bold; color: white')
         
         self.number_temperature = self.grid_upper_mid.add(_g.NumberBox(
             value=-273.16, suffix='°C', tip='Last recorded temperature value.'),
-            alignment=1).set_width(175).disable().set_style(style_2)
+            alignment=1).set_width(175).disable().set_style('font-size: 17pt; font-weight: bold; color: white')
         
         #
         self.label_temperature_status = self.grid_upper_mid.add(_g.Label(name),
@@ -837,7 +839,7 @@ class program():
         # Remember the name
         self.name  = name
         
-         
+        # If not a custom program, grab all the details
         if name != 'Custom':
             
             # Get the program steps
@@ -852,7 +854,7 @@ class program():
             # Remeber number of steps in the program
             self.size = len(self.steps)
             
-            # Get the program length (in seconds)
+            # Calculate the total program length (in seconds)
             self.length = 0
             for i in range(self.size):
                 # Add the length of this step to the total program length
@@ -860,39 +862,84 @@ class program():
         else:
             self.steps = []
 
-            
     def get_name(self):
+        """
+        Get the program name.
+        """
         return self.name
     
     def get_step_operation(self):
+        """
+        Get the operation of the current step.
+
+        """
         return self.loaded_step[0]
     
     def get_step_temperature(self):
+        """
+        Get the set temperature of the current step.
+        """
         return self.loaded_step[1]
     
     def get_step_duration(self):
+        """
+        Get the duration of the current step.
+        """
         return self.loaded_step[2]
     
     def get_size(self):
+        """
+        Get the program size (number of steps).
+        """
         return self.size
         
     def get_step(self,i):
+        """
+        Get the ith step of the program.
+
+        Parameters
+        ----------
+        i : int
+            Step index.
+
+        Returns None if no step of the requested index exists.
+
+        """
         if i < self.size:
             return self.steps[i]
         else:
             return None
     
     def get_length(self):
+        """
+        Get the full program duration (duration of all combined steps)
+        in seconds.
+        """
         return self.length  
     
     def get_step_index(self):
+        """
+        Get the current step index.
+        """
         return self.loaded_step_index
     
-    
     def add_step(self, new_step):
+        """
+        Add a new step to the program.
+
+        Parameters
+        ----------
+        new_step : list
+            List containing step information in the order
+            (operation, temperature, duration).
+        """
+        
+        # Append the new step to the step list
         self.steps.append(new_step)
         
+        # If this is the first added step
         if len(self.steps) == 1:
+            
             # Load the first step
             self.loaded_step = self.steps[0]
             
@@ -902,23 +949,41 @@ class program():
         # Update number of steps in the program
         self.size = len(self.steps)
             
-        # Update the program length (in seconds)
+        # Update the total program length (in seconds)
         self.length = 0
         for i in range(self.size):
             # Add the length of this step to the total program length
             self.length += self.steps[i][2]*3600
             
-    
     def check_next(self):
+        """
+        Check if there is a next step 
+        (following the current loaded step) to the program.
+
+        Returns
+        -------
+        bool
+            True if a next step exists, False otherwise.
+
+        """
         if self.loaded_step_index < self.size - 1:
             return True
         else:
             return False
     
     def increment_step(self):
+        """
+        Move to the next program step.
+
+        """
+        
+        # Load in the nxt program step.
         self.loaded_step = self.steps[self.loaded_step_index+1]
+        
+        # Update the step index.
         self.loaded_step_index += 1
         
+        # Calculate temperature ramping parameters
         if self.get_step_operation() == 'Ramp':
             self.dT        = self.get_step_temperature() - self.setpoint
             self.sgn       = 1 if self.dT > 0 else -1
@@ -926,6 +991,10 @@ class program():
             self.t_next    = self.step_time
         
     def run(self, current_temperature):
+        """
+        Run the program.
+        """
+        # Calculate temperature ramping parameters
         if self.get_step_operation() == 'Ramp':
             self.setpoint  = current_temperature
             
@@ -943,17 +1012,33 @@ class program():
         return self.setpoint
     
     def save_program(self):
+        """
+        Save the program to the PROGRAM_DIR directory.
+
+        """
+        
+        # Create a spinmob databox to hold the program step data
         s = _s.data.databox()
         
+        # Get the name of the new program via save dialog box.
         result = _s._qtw.QFileDialog.getSaveFileName(directory=_os.getcwd()+'/'+PROGRAM_DIR)
         self.name = result[0].split('/')[-1]
         
+        # Add a name header to the databox
         s.h(name = self.name)
 
+        # Add the program steps as headers to the databox
         for i in range(0,self.size):
+            
+            # Insert filled steps
             s.insert_header('op%d'%i, self.steps[i])
+            
         for j in range(self.size,PROGRAM_STEPS):
+            
+            # Insert empty steps
             s.insert_header('op%d'%j, '')
+            
+        # Save the file.
         s.save_file(PROGRAM_DIR+'/'+result[0].split('/')[-1])
 
         return self.name
